d51fdfa58e018d4720d6d206a0a4fbe8020a4ccf
dimesh-dev <dimeshmendis123@gmail.com>
2025-09-17 21:20:33 +0530

    Update image display to use container width in the Dog Breed Classification app

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
     image_resized = image.resize((img_width, img_height))
 
-    st.image(image, caption="Uploaded Image", width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     # Preprocess
     img_array = tf.keras.utils.img_to_array(image_resized)
